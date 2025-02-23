830f0530bdd0b6e4c9c42afe9946e436a8db946d
Vaibhav Tripathi <vaibhavxtripathi@gmail.com>
2025-02-23 21:07:07 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./styles/App.css";
-import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
 import ProjectSection from "./components/ProjectSection";
 import Contact from "./components/Contact";
 import About from "./components/About";
 import TechStacks from "./components/TechStacks";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
