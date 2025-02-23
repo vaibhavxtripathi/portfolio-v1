c13677b48645608fbdce47c502eebcb10257b04a
Vaibhav Tripathi <vaibhavxtripathi@gmail.com>
2025-02-24 00:10:28 +0530

    fast

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,63 +1,61 @@
 import React from "react";
 import Projects from "./helper/Projects";
 
-const MagneticHover = lazy(() => import("./ui/useMagneticHover"));
+import MagneticHover from "./ui/useMagneticHover";
 
 const ProjectSection = () => {
   return (
-    <Suspense fallback={<h1>Loading...</h1>}>
-      <div className="w-full max-w-6xl mx-auto mt-72 px-5 md:px-10 lg:px-16 relative">
-        {/* Title */}
-        <h1
-          id="projects"
-          className="text-4xl xs:text-[70px] xs:-top-10 sm:text-6xl md:text-7xl lg:text-8xl font-[650] absolute lg:-top-20 z-10"
-        >
-          Projects.
-        </h1>
+    <div className="w-full max-w-6xl mx-auto mt-72 px-5 md:px-10 lg:px-16 relative">
+      {/* Title */}
+      <h1
+        id="projects"
+        className="text-4xl xs:text-[70px] xs:-top-10 sm:text-6xl md:text-7xl lg:text-8xl font-[650] absolute lg:-top-20 z-10"
+      >
+        Projects.
+      </h1>
 
-        {/* Projects Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 mt-16">
-          {/* First Row */}
-          <MagneticHover padding={100} disabled={false} magnetStrength={50}>
-            <div className="bg-[#E9C3B8] flex text-black justify-center items-center p-6 min-h-[35rem]">
-              <Projects
-                img="https://vxtr.netlify.app/.netlify/images?url=/assets/pw.webp&w=800&q=80"
-                name="Picker Wheel"
-                desc="A simple clone of the picker wheel website."
-                href1="https://vaibhavxtripathi.github.io/picker-wheel"
-                href2="https://github.com/vaibhavxtripathi/picker-wheel"
-              />
-            </div>
-          </MagneticHover>
+      {/* Projects Grid */}
+      <div className="grid grid-cols-1 md:grid-cols-2 mt-16">
+        {/* First Row */}
+        <MagneticHover padding={100} disabled={false} magnetStrength={50}>
+          <div className="bg-[#E9C3B8] flex text-black justify-center items-center p-6 min-h-[35rem]">
+            <Projects
+              img="https://vxtr.netlify.app/.netlify/images?url=/assets/pw.webp&w=800&q=80"
+              name="Picker Wheel"
+              desc="A simple clone of the picker wheel website."
+              href1="https://vaibhavxtripathi.github.io/picker-wheel"
+              href2="https://github.com/vaibhavxtripathi/picker-wheel"
+            />
+          </div>
+        </MagneticHover>
 
-          <MagneticHover padding={100} disabled={false} magnetStrength={50}>
-            <div className="bg-[#224A54] text-white flex justify-center items-center p-6 min-h-[35rem]">
-              <Projects
-                img="https://vxtr.netlify.app/.netlify/images?url=/assets/weather-app.webp&w=800&q=80"
-                name="Weather App"
-                desc="A simple weather app website using JS."
-                href1="https://vaibhavxtripathi.github.io/Weather-App"
-                href2="https://github.com/vaibhavxtripathi/Weather-App"
-              />
-            </div>
-          </MagneticHover>
+        <MagneticHover padding={100} disabled={false} magnetStrength={50}>
+          <div className="bg-[#224A54] text-white flex justify-center items-center p-6 min-h-[35rem]">
+            <Projects
+              img="https://vxtr.netlify.app/.netlify/images?url=/assets/weather-app.webp&w=800&q=80"
+              name="Weather App"
+              desc="A simple weather app website using JS."
+              href1="https://vaibhavxtripathi.github.io/Weather-App"
+              href2="https://github.com/vaibhavxtripathi/Weather-App"
+            />
+          </div>
+        </MagneticHover>
 
-          {/* Second Row */}
+        {/* Second Row */}
 
-          <MagneticHover padding={100} disabled={false} magnetStrength={50}>
-            <div className="bg-[#1A1A1A] text-white flex justify-center items-center p-6 col-span-1 md:col-span-2 py-18 lg:w-[200%] xs:w-[100%] min-h-[30rem]">
-              <Projects
-                img="https://vxtr.netlify.app/.netlify/images?url=/assets/savoney.webp&w=800&q=80"
-                name="Savoney"
-                desc="A clone of the Savoney Expense Tracker website."
-                href1="https://vaibhavxtripathi.github.io/savoney"
-                href2="https://github.com/vaibhavxtripathi/savoney"
-              />
-            </div>
-          </MagneticHover>
-        </div>
+        <MagneticHover padding={100} disabled={false} magnetStrength={50}>
+          <div className="bg-[#1A1A1A] text-white flex justify-center items-center p-6 col-span-1 md:col-span-2 py-18 lg:w-[200%] xs:w-[100%] min-h-[30rem]">
+            <Projects
+              img="https://vxtr.netlify.app/.netlify/images?url=/assets/savoney.webp&w=800&q=80"
+              name="Savoney"
+              desc="A clone of the Savoney Expense Tracker website."
+              href1="https://vaibhavxtripathi.github.io/savoney"
+              href2="https://github.com/vaibhavxtripathi/savoney"
+            />
+          </div>
+        </MagneticHover>
       </div>
-    </Suspense>
+    </div>
   );
 };
 
